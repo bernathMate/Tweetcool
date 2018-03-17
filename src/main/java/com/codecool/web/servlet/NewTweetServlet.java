3a32a0d7982dca1fc411cf93a12d72c3bbea76a2
Bernáth Máté <bernath.mate.new@gmail.com>
2018-03-17 15:23:32 +0100

    The xml saving and loading implementation is started.

--- src/main/java/com/codecool/web/servlet/NewTweetServlet.java
+++ src/main/java/com/codecool/web/servlet/NewTweetServlet.java
@@ -1,11 +1,22 @@
 package com.codecool.web.servlet;
 
 import com.codecool.web.model.Tweet;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -20,13 +31,55 @@ public class NewTweetServlet extends HttpServlet{
 
         if (poster.equals("") || post.equals("")) {
             PrintWriter out = res.getWriter();
-            String htmlResponse = "<h1>ERROR: Please type your name, and your post!";
+            String htmlResponse = "<a href=\"index.html\"/>Go back</a>";
+            htmlResponse += "<h1>ERROR: Please type your name, and your post!";
             out.println(htmlResponse);
             return;
         }
 
+        try {
+            String contextPath = req.getSession().getServletContext().getRealPath("/" + "tweets.xml");
+            saveTweetToXml(new Tweet(poster, post), contextPath);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
         new TweetServlet().getTweetService().getTweets().add(new Tweet(poster, post));
         System.out.println("New post is added!");
         res.sendRedirect("index.html");
+    }
+
+    private void saveTweetToXml(Tweet tweet, String contextPath) throws IOException, SAXException, ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(contextPath);
+        Element root = document.getDocumentElement();
+
+        Element newTweet = document.createElement("Tweet");
+        //newTweet.setAttribute("id", String.valueOf(tweet.getId()));
+
+        Element poster = document.createElement("Poster");
+        poster.appendChild(document.createTextNode(tweet.getPoster()));
+        newTweet.appendChild(poster);
+
+        Element post = document.createElement("Post");
+        post.appendChild(document.createTextNode(tweet.getPost()));
+        newTweet.appendChild(post);
+
+        Element time = document.createElement("Time");
+        time.appendChild(document.createTextNode(tweet.getTimestamp()));
+        newTweet.appendChild(time);
+
+        root.appendChild(newTweet);
+
+        DOMSource source = new DOMSource(document);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        StreamResult result = new StreamResult(contextPath);
+        transformer.transform(source, result);
     }
 }
--- src/main/java/com/codecool/web/servlet/TweetServlet.java
+++ src/main/java/com/codecool/web/servlet/TweetServlet.java
@@ -1,18 +1,29 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.model.Tweet;
 import com.codecool.web.service.TweetService;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
 
 @WebServlet("/tweets")
 public class TweetServlet extends HttpServlet {
 
     private final TweetService tweetService = TweetService.getInstance();
+    //private String filePath = "resources/tweets.xml";
 
     public TweetService getTweetService() {
         return tweetService;
@@ -20,7 +31,46 @@ public class TweetServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //String contextPath = req.getSession().getServletContext().getRealPath("/" + "tweets.xml");
+        //loadTweetsFromXml(contextPath);
         req.setAttribute("tweets", tweetService.getTweets());
         req.getRequestDispatcher("tweets.jsp").forward(req, resp);
     }
+
+    private void loadTweetsFromXml(String filePath) {
+        try {
+            Document doc = createDoc(filePath);
+            doc.getDocumentElement().normalize();
+            NodeList tweets = doc.getElementsByTagName("Tweet");
+
+            for (int i = 0; i < tweets.getLength(); i++) {
+                Node tweet = tweets.item(i);
+                if (tweet.getNodeType() == Node.ELEMENT_NODE) {
+                    Element tweetElement = (Element) tweet;
+                    String id = tweetElement.getAttribute("id");
+                    String poster = tweetElement.getElementsByTagName("Poster").item(0).getTextContent();
+                    String post = tweetElement.getElementsByTagName("Post").item(0).getTextContent();
+                    String time = tweetElement.getElementsByTagName("Time").item(0).getTextContent();
+                    //tweetService.getTweets().add(new Tweet(Integer.parseInt(id), poster, post, time));
+                    new TweetService().getTweets().add(new Tweet(Integer.parseInt(id), poster, post, time));
+                }
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Document createDoc(String filename) throws ParserConfigurationException, IOException, SAXException {
+        File xmlFile = new File(filename);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(xmlFile);
+        return doc;
+    }
+
+
 }
